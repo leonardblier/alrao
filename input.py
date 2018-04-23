e7876abbe3d8cc290ad0f914f61b1c70df5452e5
Pierre Wolinski <pierre.wolinski@inria.fr>
2018-04-23 21:33:14 +0200

    Ajout des fichiers de base pour le lancement d'expes

---
+++ input.py
@@ -0,0 +1,69 @@
+import sys
+import argparse
+from collections import defaultdict
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='weight-decay')
+
+    # CUDA
+    parser.add_argument('--no-cuda', action = 'store_true', default = False, \
+                        help = 'disable cuda')
+
+    # epochs
+    parser.add_argument('--epochs', type = int, default = 1, \
+                        help = 'number of epochs for phase 1 (default: 1)')
+    parser.add_argument('--epochs2', type = int, default = 0, \
+                        help = 'number of epochs for phase 2 (default: 0)')
+
+    # Drop out
+    parser.add_argument('--dropOut', type = float, default = 0, \
+                        help = 'drop-out rate in the first phase (default: 0)')
+    parser.add_argument('--dropOut2', type = float, default = 0, \
+                        help = 'drop-out rate in the second phase (default: 0)')
+
+    # prior
+    parser.add_argument('--penalty', action = 'store_true', default = False, \
+                        help = 'add penalty to loss')
+
+    # options
+    parser.add_argument('--optimizer', default = 'Adam', \
+                        help = 'optimizer (default: Adam) {Adam, SGD, RMSprop}')
+    parser.add_argument('--lr', type = float, default = .01, \
+                        help = 'learning rate (default: .01)')
+    parser.add_argument('--suffix', default = '', \
+                        help = 'suffix for the file name')
+    parser.add_argument('--exp_number', type = int, default = -1, \
+                        help = 'for seq jobs: number of the run')
+    parser.add_argument('--data_augm', action = 'store_true', default = False, \
+                        help = 'add data augmentation')
+    parser.add_argument('--mLR', action = 'store_true', default = False, \
+                        help = 'multiple learning rates')
+    parser.add_argument('--minLR', type = int, default = 0, \
+                        help = 'log of the minimum LR')
+    parser.add_argument('--maxLR', type = int, default = 0, \
+                        help = 'log of the maximum LR')
+    parser.add_argument('--switch', type = int, default = 0, \
+                        help = 'number of layers before the switch')
+    parser.add_argument('--stats', action = 'store_true', default = False, \
+                        help = 'outputs PDF with stats over weights')
+    parser.add_argument('--nb_neur_mult', type = float, default = 1, \
+                        help = 'multiplier of the number of neurons per layer (default: 1)')
+
+    # print options
+    parser.add_argument('--print_period', type = int, default = 10, \
+                        help = 'sort period (default: 10)')
+    parser.add_argument('--print_TWeights', action = 'store_true', default = False, \
+                        help = 'periodic output of TWeights files')
+
+    # batchnorm
+    parser.add_argument('--batchnorm', default = 'None', \
+                        help = 'batchnorm (default: None) {None, BB, BN, BNP}')
+    parser.add_argument('--BN_lr', type = float, default = .1, \
+                        help = 'batchnorm learning rate (default: .1)')
+    parser.add_argument('--BN_affine', action = 'store_true', default = False, \
+                        help = 'activate linear layer after batchnorm')
+
+    args = parser.parse_args()
+
+    return args
---
+++ sbatch.py
@@ -0,0 +1,38 @@
+import sys
+import subprocess
+
+# General definitions
+def build_args(argsDict):
+    lstArgs = ''
+    for name, key in argsDict.items():
+        if isinstance(key, bool):
+            if key:
+                lstArgs += ' --' + name
+        elif isinstance(key, str):
+            if key != '':
+                lstArgs += ' --' + name + '=' + key
+        else:
+            lstArgs += ' --' + name + '=' + repr(key)
+    return lstArgs
+
+def build_sbatch(py_file_name, temp_file_name, sbatchOpt, argsDict):
+    f_sbatch = open(temp_file_name, 'w')
+    f_sbatch.write('#!/bin/bash\n\n')
+
+    for opt in sbatchOpt:
+        f_sbatch.write('#SBATCH ' + opt + '\n')
+    f_sbatch.write('\n')
+
+    f_sbatch.write('python ' + py_file_name + build_args(argsDict) + '\n')
+    f_sbatch.close()
+
+def launch_exp(py_file_name, temp_file_name, sbatchOpt, argsDict):
+    build_sbatch(py_file_name, temp_file_name, sbatchOpt, argsDict)
+    bashCMD = 'sbatch ' + temp_file_name
+    proc = subprocess.Popen(bashCMD.split(), stdout = subprocess.PIPE)
+    output, error = proc.communicate()
+
+    if not output is None:
+        print(str(output, 'utf-8').strip())
+    if not error is None:
+        print(str(error, 'utf-8').strip(), file = sys.stderr)
