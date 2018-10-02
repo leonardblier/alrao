817c4bc4c14136abb783b9237164aa40c7687c1c
Pierre Wolinski <pierre.wolinski@inria.fr>
2018-10-02 11:50:42 +0200

    Added comments in learningratesgen.py

--- learningratesgen.py
+++ learningratesgen.py
@@ -8,12 +8,23 @@ import pdb
 
 
 def lr_sampler_generic(minlr, maxlr):
+    """
+    Generates a funcion sampling random tensors according to the 'log-uniform' distribution.
+
+    Log-uniform (LU) distribution:
+        Z ~ U(log(a), log(b))
+        X = exp(Z)
+        Y = LU(a, b)
+        # X and Y have the same distribution
+
+    Arguments:
+        minlr, maxlr: bounds of the log-uniform distribution
+    """
     def f(tensor, size):
         """
         Takes a torch tensor as input and sample a tensor with same size for
-        learning rates
+        learning rates.
         """
-
         lr = tensor.new(size).uniform_()
         lr = (lr * (np.log(maxlr) - np.log(minlr)) + np.log(minlr)).exp()
         #lr.fill_(base_lr)
@@ -21,9 +32,15 @@ def lr_sampler_generic(minlr, maxlr):
 
     return f
 
+def generator_randomlr_neurons(module, lr_sampler, memo = None):
+    """
+    Generates tensors of learning rates for each submodule of the input module.
+    NOTE: the learning rates are the same for each weight in a given neuron
 
-
-def generator_randomlr_neurons(module, lr_sampler, memo=None, same_lr=1):
+    Arguments:
+        module: the pre-classifier to be trained with Alrao
+        lr_sampler: function sampling the learning rates
+    """
     if memo is None:
         memo = set()
 
@@ -35,7 +52,7 @@ def generator_randomlr_neurons(module, lr_sampler, memo=None, same_lr=1):
         for k in range(w.size()[0]):
             lrw[k].fill_(lrb[k])
         yield lrw
-        
+
         if module.bias is not None:
             memo.add(module.bias)
             yield lrb
@@ -51,22 +68,21 @@ def generator_randomlr_neurons(module, lr_sampler, memo=None, same_lr=1):
         for k in range(w.size(1)):
             lrw[k].fill_(lrb[k])
         lrw = lrw.t()
-        
+
         yield lrw
 
-        
     elif isinstance(module, nn.LSTM):
         dct_lr = {}
         for name, p in module._parameters.items():
             if name.find('weight_ih') == 0:
                 memo.add(p)
                 lrb_ih = lr_sampler(p, p.size()[:1])
-            
+
                 lrw_ih = p.new(p.size())
                 for k in range(p.size(0)):
                     lrw_ih[k].fill_(lrb_ih[k])
                 yield lrw_ih
-                
+
             elif name.find('weight_hh') == 0:
                 memo.add(p)
                 lrb_hh = lr_sampler(p, p.size()[:1])
@@ -84,29 +100,36 @@ def generator_randomlr_neurons(module, lr_sampler, memo=None, same_lr=1):
                 print("switch module: optim_spec.py: WARNING: UNKNOWN PARAMETER IN LSTM MODULE: {}".format(name))
         return
 
-            
     for _, p in module._parameters.items():
         if p is not None and p not in memo:
             print("WARNING:NOTIMPLEMENTED LAYER:{}".format(type(module)))
             memo.add(p)
             plr = lr_sampler(p, p.size())
             yield plr
-            
+
     for mname, module in module.named_children():
         for lr in generator_randomlr_neurons(module, lr_sampler, memo):
             yield lr
 
 
 def generator_randomlr_weights(module, lr_sampler, memo=None):
+    """
+    Generates tensors of learning rates for each submodule of the input module.
+    NOTE: every weight is associated to its own learning rate
+
+    Arguments:
+        module: the pre-classifier to be trained with Alrao
+        lr_sampler: function sampling the learning rates
+    """
     if memo is None:
         memo = set()
-            
+
     for _, p in module._parameters.items():
         if p is not None and p not in memo:
             memo.add(p)
             plr = lr_sampler(p, p.size())
             yield plr
-            
+
     for mname, module in module.named_children():
         for lr in generator_randomlr_weights(module, lr_sampler, memo):
             yield lr
@@ -129,19 +152,19 @@ def generator_randomdir_neurons(module, lr_sampler, memo=None):
         Hb = np.dot(np.dot(basis.T, lrb), basis)
         Hb = w.new_tensor(Hb)
         yield Hb
-        
+
         if module.bias is not None:
             memo.add(module.bias)
             yield Hb
         return
-            
+
     for _, p in module._parameters.items():
         if p is not None and p not in memo:
             print("WARNING:NOTIMPLEMENTED LAYER:{}".format(type(module)))
             memo.add(p)
             plr = lr_sampler(p, p.size())
             yield plr
-            
+
     for mname, module in module.named_children():
         for lr in generator_randomdir_neurons(module, lr_sampler, memo):
             yield lr
@@ -150,21 +173,21 @@ def generator_randomdir_neurons(module, lr_sampler, memo=None):
 def generator_randomdir_weights(module, lr_sampler, memo=None):
     if memo is None:
         memo = set()
-            
+
     for _, p in module._parameters.items():
         if p is not None and p not in memo:
             memo.add(p)
 
             size = int(np.prod(p.size()))
             print("Generating random directions of size {}".format(size))
-            
+
             basis, _ = np.linalg.qr(np.random.randn(size, size))
             plr = np.diag(lr_sampler(p.cpu(), torch.Size([size])).numpy())
             Hb = np.dot(np.dot(basis.T, plr), basis)
-            
+
             Hb = p.new_tensor(Hb)
             yield Hb
-            
+
     for mname, module in module.named_children():
         for lr in generator_randomdir_weights(module, lr_sampler, memo):
             yield lr
