f6f6462ed7bd6118fca0ea2465eba68b6b96da61
Leonard Blier <leonard.blier@gmail.com>
2018-10-03 16:21:41 +0200

    replace mymodels in __init__.py

--- alrao/__init__.py
+++ alrao/__init__.py
@@ -3,4 +3,4 @@ from .optim_spec import SGDAlrao, AdamAlrao
 from .gen_hyper import *
 from .learningratesgen import lr_sampler_generic, generator_randomlr_neurons, generator_randomlr_weights
 from .alrao_model import AlraoModel
-from .mymodels import *
+from .custom_layers import *
