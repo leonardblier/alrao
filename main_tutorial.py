bc3a8607407348d44e03bac167e733cb82738c8d
Leonard Blier <leonard.blier@gmail.com>
2018-10-03 18:06:21 +0200

    .

--- main_tutorial.py
+++ main_tutorial.py
@@ -1,4 +1,4 @@
-'''Train PTB with PyTorch.'''
+'''Train CIFAR with PyTorch.'''
 from __future__ import print_function
 from collections import OrderedDict
 
@@ -37,10 +37,10 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_train)
+trainset = torchvision.datasets.CIFAR10(root='./datasets', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
+testset = torchvision.datasets.CIFAR10(root='./datasets', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 # Pre-classifier class
