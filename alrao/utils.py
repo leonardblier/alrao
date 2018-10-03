37fc7b9f560c6e8d4f0b145bbb6b18748fc22faf
Leonard Blier <leonard.blier@gmail.com>
2018-10-03 11:48:19 +0200

    small modifs

--- alrao/utils.py
+++ alrao/utils.py
@@ -1,25 +1,21 @@
 import numpy as np
 from torch.utils.data.dataset import Dataset
 
+
 def l2params(model):
+    """
+    Print the l2 norms of each parameter tensor and of its gradient with its name
+    """
     for name, p in model.named_parameters():
-        #l += p.data.pow(2).sum()
         print("{} : \tL2Params : \t {:.5f} \t L2Grad : \t {:.5f}".format(
             name,
             float(p.data.pow(2).sum()) / np.prod(p.data.size()),
             float(p.grad.data.pow(2).sum()) / np.prod(p.data.size()),
         ))
-              
+
     print("  ")
 
-def l2grad(model):
-    l = 0.
-    print("L2 GRAD")
-    for name, p in model.named_parameters():
-        #l += p.data.pow(2).sum()
-        print("{}:\t{:.3f}".format(name, p.grad.data.pow(2).sum()))
-    print("  ")
-    #return l
+
 
 
 #### TO BE REMOVED
