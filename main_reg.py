e1686b850bd087d5eb93fc4ccc35b265df04c914
Pierre Wolinski <pierre.wolinski@inria.fr>
2019-01-11 23:36:25 +0100

    debugging...

--- main_reg.py
+++ main_reg.py
@@ -145,9 +145,7 @@ class L2LossAdditional(_Loss):
         probas_per_cl = probas_per_cl.transpose(0, 1)
         # probas_per_cl: batch_size * nb_classifiers
         probas = (probas_per_cl * ps).sum(1) / math.sqrt(2 * math.pi * self.sigma2)
-        if probas.log().mean() != probas.log().mean():
-            print(probas)
-            print('ALERTE !!!')
+        print(probas.log().mean())
         return -probas.log().mean()
         """
         mu_i, pi_i = input
